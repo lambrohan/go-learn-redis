97198857c065bae26b9a260c09c8c62bca06a889
Rohan Lamb <lambrohan@gmail.com>
2024-04-17 21:05:34 +0200

    stage 2

--- app/server.go
+++ app/server.go
@@ -18,9 +18,35 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Failed to accept connection")
+			os.Exit(1)
+		}
+
+		handleConnection(conn)
+
 	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Failed to read data")
+		return
+	}
+
+	_, err = conn.Write([]byte("+PONG\r\n"))
+
+	if err != nil {
+		fmt.Println("Failed to write data")
+		return
+	}
+
 }
